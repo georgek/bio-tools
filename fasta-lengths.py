132b061fe0c754e2fdd621871093d1d18d6d4a8f
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2015-01-28 16:20:16 +0000

    Add fasta-lengths.py

---
+++ fasta-lengths.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import argparse
+
+# ----- command line parsing -----
+parser = argparse.ArgumentParser(description="Prints lengths of contigs in Fasta.")
+parser.add_argument("file", type=str, help="Fasta file.")
+
+args = parser.parse_args()
+# ----- end command line parsing -----
+
+fasta = open(args.file)
+
+length = None
+for line in fasta:
+    if line[0] == '>':
+        if length:
+            print length
+        length = 0
+    else:
+        length += len(line)-1
