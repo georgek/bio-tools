eff1b5a02c09a780bc7462fb41729098b5cd1591
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2015-03-24 11:58:24 +0000

    Add fasta-filter

--- fasta-filter.py
+++ fasta-filter.py
@@ -9,22 +9,20 @@ import re
 parser = argparse.ArgumentParser(
     description="Prints fasta containing only the given set of sequences.")
 parser.add_argument("fasta_file", type=str, help="FASTA file.")
-parser.add_argument("regex", type=str, help="Regular expression.")
+parser.add_argument("prefix", type=str, help="Prefix of names.")
 
 args = parser.parse_args()
 # ----- end command line parsing -----
 
 fasta_file = open(args.fasta_file)
-regex = re.compile(args.regex)
 
 printing = False
 for line in fasta_file:
     if line[0] == '>':
-        if re.search(regex, line[1:-1]):
+        if prefix in line[1:-1]:
             printing = True
             sys.stdout.write(line)
         else:
             printing = False
     elif printing:
         sys.stdout.write(line)
-
