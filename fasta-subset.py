2ebd2e56359446d8245dce925d00e2669c945cd6
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2015-03-24 15:50:43 +0000

    Speed up fasta subset

--- fasta-subset.py
+++ fasta-subset.py
@@ -5,12 +5,16 @@
 import sys
 import os.path
 import argparse
+from sets import Set
 
 # ----- command line parsing -----
 parser = argparse.ArgumentParser(
     description="Prints fasta containing only the given set of sequences.")
 parser.add_argument("fasta_file", type=str, help="FASTA file.")
 parser.add_argument("sample_names", type=str, help="Sample names.")
+parser.add_argument("-p", "--prefix", dest="prefix", action="store_true",
+                    help="Names given are a prefix of the name in the fasta.")
+parser.set_defaults(prefix=False)
 
 args = parser.parse_args()
 # ----- end command line parsing -----
@@ -18,15 +22,19 @@ args = parser.parse_args()
 fasta_file = open(args.fasta_file)
 sample_names_file = open(args.sample_names)
 
-sample_names = []
+sample_names = Set()
 
 for line in sample_names_file:
-    sample_names.append(line[:-1])
+    sample_names.add(line[:-1])
 
 printing = False
 for line in fasta_file:
     if line[0] == '>':
-        if line[1:-1] in sample_names:
+        if args.prefix:
+            name = line[1:-1].split()[0]
+        else:
+            name = line[1:-1]
+        if name in sample_names:
             printing = True
             sys.stdout.write(line)
         else:
