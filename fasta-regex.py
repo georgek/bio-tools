0e4075201161b766dda440bf15d5982f57f56722
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2015-05-06 12:07:39 +0100

    fasta-regex.py: add invert option

--- fasta-regex.py
+++ fasta-regex.py
@@ -10,6 +10,9 @@ parser = argparse.ArgumentParser(
     description="Prints fasta containing only the given set of sequences.")
 parser.add_argument("fasta_file", type=str, help="FASTA file.")
 parser.add_argument("regex", type=str, help="Regular expression.")
+parser.add_argument("-v", "--invert-match", dest="invert", action="store_true",
+                    help="Invert the sense of matching, to select non-matching sequences.")
+parser.set_defaults(invert=False)
 
 args = parser.parse_args()
 # ----- end command line parsing -----
@@ -20,7 +23,7 @@ regex = re.compile(args.regex)
 printing = False
 for line in fasta_file:
     if line[0] == '>':
-        if re.search(regex, line[1:-1]):
+        if bool(re.search(regex, line[1:-1])) ^ bool(args.invert):
             printing = True
             sys.stdout.write(line)
         else:
