18b7f8de264adebcda86ba97ef1dbf7a10b6f053
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2016-07-28 13:38:22 +0100

    sam-to-svg.py: alignment length based on cigar

--- sam-to-svg.py
+++ sam-to-svg.py
@@ -3,6 +3,19 @@
 import sys
 import argparse
 
+def cigar_to_aln_len(cigar):
+    current_number = ""
+    total_length = 0
+    for c in cigar:
+        if c in "0123456789":
+            current_number += c
+        elif c in "MD=X":
+            total_length += int(current_number)
+            current_number = ""
+        else:
+            current_number = ""
+    return total_length
+
 # ----- command line parsing -----
 parser = argparse.ArgumentParser(
     prog="sam-to-svg",
@@ -28,17 +41,22 @@ for line in file:
                     length = int(bit[3:])
     else:
         [qname,flag,rname,pos,mapq,cigar,rnext,pnext,tlen,seq,qual,opt] = line.split(None,11)
+        aln_len = cigar_to_aln_len(cigar)
         inserted = False
         for lane in lanes:
             if len(lane) == 0:
-                lane.append((int(pos),len(seq)))
+                lane.append((int(pos),aln_len))
                 inserted = True
             elif lane[-1][0]+lane[-1][1] < int(pos):
-                lane.append((int(pos),len(seq)))
+                lane.append((int(pos),aln_len))
                 inserted = True
                 break
         if not inserted:
-            lanes.append([(int(pos),len(seq))])            
+            lanes.append([(int(pos),aln_len)])
+
+if length is None:
+    sys.stderr.write("Error: ref length must be given is SAM header is not present (--ref_length)\n")
+    exit(1)
 
 height = len(lanes) * 10 + (len(lanes)-1) * 2
 sys.stdout.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
@@ -50,7 +68,9 @@ y = 0
 for lane in lanes:
     for aln in lane:
         sys.stdout.write("    <rect x=\"{:f}\" y=\"{:d}\" width=\"{:f}\" height=\"10\" />\n"
-                         .format((float(aln[0])/length)*100, y, (float(aln[1])/length)*100))
+                         .format((float(aln[0])/length)*100,
+                                 y,
+                                 (float(aln[1])/length)*100))
     y += 12
 
 sys.stdout.write("  </g>\n")
