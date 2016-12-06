64cfe36e7f81f0926ebb694909255be18b634593
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2016-12-06 10:35:40 +0000

    fastq-subsample: take original coverage and warn is new not possible

--- fastq-subsample.cc
+++ fastq-subsample.cc
@@ -6,6 +6,8 @@
 
 int main(int argc, char *argv[])
 {
+    double original_coverage;
+    double new_coverage;
     double divide_coverage;
     int old_read_length;
     int new_read_length;
@@ -15,28 +17,38 @@ int main(int argc, char *argv[])
 
     std::ios_base::sync_with_stdio(false);
 
-    if (argc < 4
+    if (argc < 5
         || argc > 1
            && (strcmp(argv[1], "-h") == 0
                || strcmp(argv[1], "--help") == 0)) {
         std::cerr
             << "usage: " << argv[0] << " [-h]"
-" divide_coverage old_read_length new_read_length [random_seed]\n\n"
+" original_coverage new_coverage old_read_length new_read_length [random_seed]\n\n"
 "    Reads fastq and from standard input and prints subsampled version to\n"
 "    standard output. Coverage is divided by divide_coverage.\n"
 "    If using paired-end, do both R1 and R2 with the same random seed."
             << std::endl;
         std::exit(0);
     }
-    divide_coverage = std::stod(argv[1]);
-    old_read_length = std::stoi(argv[2]);
-    new_read_length = std::stoi(argv[3]);
+    original_coverage = std::stod(argv[1]);
+    new_coverage = std::stod(argv[2]);
+    divide_coverage = original_coverage/new_coverage;
+    old_read_length = std::stoi(argv[3]);
+    new_read_length = std::stoi(argv[4]);
     len_ratio = new_read_length/(double) old_read_length;
     cov_ratio = 1.0/divide_coverage/len_ratio;
+    if (cov_ratio > 1.0) {
+        std::cerr << "Warning: " << new_coverage << "x coverage requested, but only "
+                  << new_coverage/cov_ratio << "x possible." << std::endl;
+    }
+    if (new_read_length > old_read_length) {
+        std::cerr << "Warning: new read length longer than old read length,"
+            "you will only get " << original_coverage*cov_ratio << "x" << std::endl;
+    }
 
     std::mt19937 mt;
-    if (argc > 4) {
-        random_seed = std::stod(argv[4]);
+    if (argc > 5) {
+        random_seed = std::stod(argv[5]);
         mt = std::mt19937(random_seed);
     }
     else {
