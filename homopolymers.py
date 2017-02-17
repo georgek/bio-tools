56f8bfc54da95772d4fc6e9c8cf2d13d7bfa7926
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2017-02-17 16:59:58 +0000

    80 char formatting

--- homopolymers.py
+++ homopolymers.py
@@ -14,13 +14,15 @@ def progress(fp, fs, fin):
     prog = 0
     while not fin.isSet():
         if sys.stderr.isatty():
-            sys.stderr.write("\r{:3.0f}% {:s}\b".format(fp.tell()/float(fs)*100,
-                                                        progress[prog]))
+            sys.stderr.write("\r{:3.0f}% {:s}\b"
+                             .format(fp.tell()/float(fs)*100,
+                                     progress[prog]))
             sys.stderr.flush()
             prog = (prog + 1)%4
             time.sleep(0.1)
         else:
-            sys.stderr.write("{:3.0f}% ".format(fp.tell()/float(fs)*100))
+            sys.stderr.write("{:3.0f}% "
+                             .format(fp.tell()/float(fs)*100))
             time.sleep(5)
     else:
         if sys.stderr.isatty():
@@ -32,12 +34,17 @@ def progress(fp, fs, fin):
 Homopolymer = namedtuple("Homopolymer", "seqname base length beg end")
 
 # ----- command line parsing -----
-parser = argparse.ArgumentParser(description="Finds positions of homopolyomers.")
-parser.add_argument("file", type=str, help="Fasta file.")
-parser.add_argument("min_length", type=int, help="Minimum length of homopolymer.")
-parser.add_argument("-s", "--case_sensitive", dest="case", action="store_true",
+parser = argparse.ArgumentParser(
+    description="Finds positions of homopolyomers.")
+parser.add_argument("file", type=str,
+                    help="Fasta file.")
+parser.add_argument("min_length", type=int,
+                    help="Minimum length of homopolymer.")
+parser.add_argument("-s", "--case_sensitive",
+                    dest="case", action="store_true",
                     help="Case sensitive bases.")
-parser.add_argument("-b", "--bases", type=str, help="Comma separated list of bases to consider.")
+parser.add_argument("-b", "--bases", type=str,
+                    help="Comma separated list of bases to consider.")
 parser.set_defaults(case=False)
 
 args = parser.parse_args()
@@ -62,7 +69,9 @@ try:
     for line in fasta:
         if line[0] == '>':
             if run >= args.min_length:
-                    homopolymers.append(Homopolymer(name, last_base, run, pos-run+1, pos))
+                    homopolymers.append(
+                        Homopolymer(name, last_base,
+                                    run, pos-run+1, pos))
             name = line[1:-1]
             pos = 0
             run = 0
@@ -78,11 +87,15 @@ try:
                 run += 1
             else:
                 if run >= args.min_length:
-                    homopolymers.append(Homopolymer(name, last_base, run, pos-run, pos-1))
+                    homopolymers.append(
+                        Homopolymer(name, last_base,
+                                    run, pos-run, pos-1))
                 run = 1
                 last_base = base
     if run >= args.min_length:
-        homopolymers.append(Homopolymer(name, last_base, run, pos-run+1, pos))
+        homopolymers.append(
+            Homopolymer(name, last_base,
+                        run, pos-run+1, pos))
     fin.set()
     pthread.join()
 except KeyboardInterrupt:
@@ -103,4 +116,6 @@ else:
 
 for homo in homopolymers:
     if bases is None or homo.base in bases:
-        sys.stdout.write("{:s}\t{:s}\t{:d}\t{:d}\t{:d}\n".format(*homo))
+        sys.stdout.write("{:s}\t{:s}\t{:d}\t{:d}\t{:d}\n"
+                         .format(*homo))
+
