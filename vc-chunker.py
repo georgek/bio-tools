c398b4238fa1df210d783882b99ef809347f7488
George Kettleborough <george.kettleborough@earlham.ac.uk>
2017-08-24 10:58:27 +0100

    vc-chunker: support multiple sample inputs

--- vc-chunker.py
+++ vc-chunker.py
@@ -15,7 +15,7 @@ def get_args():
         description="Make jobs for variant calling on an alignment with split regions.")
     parser.add_argument("ref_file", type=str,
                         help="Reference FASTA file.")
-    parser.add_argument("aln_file", type=str,
+    parser.add_argument("aln_files", type=str, nargs="+", metavar="aln_file",
                         help="Alignment file.")
     parser.add_argument("-o", "--output_dir", type=str, default=DEFAULT_OUTPUT_DIR,
                         help="Output directory for job files.")
@@ -48,12 +48,13 @@ source bcftools-1.3.1
 """
     return boilerplate
 
-def write_batch_script(chunk_n, ref_file, aln_file, refs, out_dir, quality=0):
-    aln_basename = aln_file.split("/")[-1]
+
+def write_batch_script(chunk_n, ref_file, aln_files, refs, out_dir, quality=0):
+    aln_basenames = [aln_file.split("/")[-1] for aln_file in aln_files]
     if args.output_prefix:
         prefix = args.output_prefix
     else:
-        prefix = aln_basename
+        prefix = "-".join(aln_basenames)
     out_name = ("{out_dir:s}/{prefix:s}_chunk_{n:05d}.job"
                 .format(out_dir=out_dir, prefix=prefix, n=chunk_n))
     mode = "w" if args.force else "x"
@@ -61,14 +62,21 @@ def write_batch_script(chunk_n, ref_file, aln_file, refs, out_dir, quality=0):
         with open(out_name, mode) as fout:
             fout.write(get_sbatch_boilerplate())
             fout.write("""
-samtools view -q{quality:d} -u {aln_file:s} {ref_string:s} \\
-    | samtools mpileup -u -t AD,DP -f {ref_file:s} - \\
-    | bcftools call -mv -Ob - \\
-    > {prefix:s}_chunk_{chunk_n:05d}.bcf
-bcftools index {prefix:s}_chunk_{chunk_n:05d}.bcf
+regions=\"{ref_string:s}\"
+pipes=\"\"
+for bam in {aln_file_string:s}; do
+   pipe=$(mktemp -u)
+   mkfifo ${{pipe}}
+   samtools view -u ${{bam}} ${{regions}} > ${{pipe}} &
+   pipes=\"${{pipes}} ${{pipe}}\"
+done
+samtools mpileup -u -q{quality:d} -t AD,DP -f {ref_file:s} ${{pipes}} \\
+   | bcftools call -c -Ob \\
+      > {prefix:s}_chunk_{chunk_n:05d}.bcf
+rm ${{pipes}}
             """.format(chunk_n=chunk_n,
                        ref_file=ref_file,
-                       aln_file=aln_file,
+                       aln_file_string=" ".join(aln_files),
                        ref_string=" ".join(refs),
                        prefix=prefix,
                        quality=quality))
@@ -78,17 +86,17 @@ bcftools index {prefix:s}_chunk_{chunk_n:05d}.bcf
 
 
 def main(ref_file,
-         aln_file,
+         aln_files,
          output_dir=DEFAULT_OUTPUT_DIR,
          output_prefix=DEFAULT_OUTPUT_PREFIX,
          chunk_size=DEFAULT_CHUNK_SIZE,
          min_quality=DEFAULT_MIN_QUALITY,
          force=False):
     try:
-        faidx_f = open(args.ref_file + ".fai")
+        faidx_f = open(ref_file + ".fai")
     except FileNotFoundError as e:
-        sys.stderr.write("Index file for " + args.ref_file + " not found.\n"
-                         "Use samtools faidx " + args.ref_file + "\n")
+        sys.stderr.write("Index file for " + ref_file + " not found.\n"
+                         "Use samtools faidx " + ref_file + "\n")
         exit()
 
     current_refs = []
@@ -100,15 +108,15 @@ def main(ref_file,
         name, length, rest = line.split(maxsplit=2)
         length = int(length)
         if current_size > 0 \
-           and ((current_size + length) > args.chunk_size \
+           and ((current_size + length) > chunk_size \
                 or (current_arg_length + len(name) + 1) > max_arg_length):
             num_chunks += 1
             write_batch_script(num_chunks,
-                               args.ref_file,
-                               args.aln_file,
+                               ref_file,
+                               aln_files,
                                current_refs,
-                               args.output_dir,
-                               args.min_quality)
+                               output_dir,
+                               min_quality)
             current_refs = []
             current_size = 0
             current_arg_length = 0
@@ -117,11 +125,11 @@ def main(ref_file,
         current_arg_length += (len(name) + 1)
     num_chunks += 1
     write_batch_script(num_chunks,
-                       args.ref_file,
-                       args.aln_file,
+                       ref_file,
+                       aln_files,
                        current_refs,
-                       args.output_dir,
-                       args.min_quality)
+                       output_dir,
+                       min_quality)
 
 
 if __name__ == '__main__':
